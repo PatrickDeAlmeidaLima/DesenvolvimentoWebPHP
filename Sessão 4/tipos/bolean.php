e49373ff5d1ac1503fce8e815d7bfa5937d619cc
unknown <patrick.almeida.lima@gmail.com>
2023-05-11 22:17:41 -0300

    continuando string com boleano

--- Sessão 4/tipos/bolean.php
+++ Sessão 4/tipos/bolean.php
@@ -14,7 +14,12 @@ echo is_bool('false') . '<br>';
 
 // fazer as regras de conversões
 echo '<p>Regras: </p>' . '<br>';
-echo var_dump((bool)0). '<br>'; //apenas zero é false
-echo var_dump((bool)1). '<br>';
-echo var_dump((bool)5). '<br>';
-echo var_dump((float)1). '<br>';
+echo var_dump((bool)0) . '<br>'; //apenas zero é false
+echo var_dump((bool)1) . '<br>';
+echo var_dump((bool)5) . '<br>';
+echo var_dump((float)1) . '<br>';
+echo var_dump((bool) -1) . '<br>';
+echo var_dump((bool) 0.0) . '<br>';
+echo var_dump((bool) 0.00000000000001) . '<br>';
+echo var_dump((bool) "") . '<br>';
+echo var_dump((bool) " ") . '<br>';

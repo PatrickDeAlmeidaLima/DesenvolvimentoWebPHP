9e915484819fd4edb27db0975fabf223c4ee6105
unknown <patrick.almeida.lima@gmail.com>
2023-06-01 21:20:28 -0300

    manipulacao de variaveis

---
+++ Sesssão 5/variaveis/atribuicoes.php
@@ -0,0 +1,20 @@
+<div class="titulo">Atribuições</div>
+
+<?php
+$title = 'Atribuições';
+
+$numero = 10;
+echo '<br>' . $numero;
+$numero = $numero - 3;
+echo '<br>' . $numero;
+$numero = $numero + 1.5;
+echo '<br>' . $numero;
+$numero--; //pós fixado numero = numero -1
+--$numero; //pre fixado numero = numero -1
+echo '<br>' . $numero;
+$numero++; //numero = numero +1
+++$numero; //numero = numero +1
+echo '<br>' . $numero;
+$numero-=5; //numero = numero +1
+$numero=55; //numero = numero +1
+echo '<br>' . $numero;

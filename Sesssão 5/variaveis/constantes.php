a045f96b41f9a03fe0dd1689452f43bc348d83d6
unknown <patrick.almeida.lima@gmail.com>
2023-08-15 21:20:47 -0300

    constante

---
+++ Sesssão 5/variaveis/constantes.php
@@ -0,0 +1,21 @@
+<div class="titulo">Constantes</div>
+
+<?php
+define('TAXA_DE_JUROS', 5.9);
+
+echo TAXA_DE_JUROS;
+
+echo '<br>' . $TAXA_DE_JUROS . '!';
+
+// TAXA_DE_JUROS = 2.9;
+
+const NOVA_TAXA = 2.5;
+
+echo '<br>' . NOVA_TAXA . '!';
+
+$valorVariavel = 2.8;
+define('NOVISSIMA_TAXA', $valorVariavel);
+// const NOVISSIMA_TAXA = $valorVariavel;
+// const NOVISSIMA_TAXA = 2.8 + 1.2;
+
+echo '<br>' . NOVISSIMA_TAXA . '!';

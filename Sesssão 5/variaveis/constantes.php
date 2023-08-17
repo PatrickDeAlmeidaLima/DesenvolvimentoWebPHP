dcceeb23517186e7f1834ef6237f99d066453c3c
unknown <patrick.almeida.lima@gmail.com>
2023-08-16 21:48:42 -0300

    constante

--- Sesssão 5/variaveis/constantes.php
+++ Sesssão 5/variaveis/constantes.php
@@ -14,8 +14,17 @@ const NOVA_TAXA = 2.5;
 echo '<br>' . NOVA_TAXA . '!';
 
 $valorVariavel = 2.8;
-define('NOVISSIMA_TAXA', $valorVariavel);
+// define('NOVISSIMA_TAXA', $valorVariavel);
 // const NOVISSIMA_TAXA = $valorVariavel;
-// const NOVISSIMA_TAXA = 2.8 + 1.2;
-
+const NOVISSIMA_TAXA = 2.8 + 1.2;
 echo '<br>' . NOVISSIMA_TAXA . '!';
+
+echo '<br>' . PHP_VERSION;
+
+echo '<br>' . PHP_INT_MAX;
+echo '<br>' . PHP_INT_MIN;
+
+echo '<br>Linha :' . __LINE__;
+
+echo'<br>'.__FILE__;
+echo'<br>'.__DIR__;

f3ac99f5704341df158cf70e033da8f91f7dcf96
unknown <patrick.almeida.lima@gmail.com>
2023-08-24 21:24:55 -0300

    continuando aula ifelse

--- Sesssão 6/controle/if_else.php
+++ Sesssão 6/controle/if_else.php
@@ -1,11 +1,36 @@
 <div class="titulo">If Else</div>
 
 <?php
-if (true) {
-    echo "Serei impresso?";
-    echo "<br>"."Serei impresso novamente?";
-} else {
+if (true) 
+    echo "Serei impresso?"."<br>";
+    echo "Serei impresso novamente?"."<br>";
 
+
+if (false) {
+    echo "Verdadeiro<br>";
+    
+} else {
+    echo "Falso<br>";
 }
+
+echo "Fim<br>";
+
+if (false) {
+    echo "Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+    
+} else {
+    echo "Falso<br> - Parte A<br>";
+    echo "Falso<br> - Parte B<br>";
+}
+
+if (false) {
+    echo "Verdadeiro - Passo C<br>";
+    
+} else {
+    echo "Último Passo - Parte C<br>";
+}
+
+echo "Fim<br>";
 
 ?>

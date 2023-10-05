a2dcf42cf6f89b9b68f1b2fe7ae19dc3f6637257
unknown <patrick.almeida.lima@gmail.com>
2023-10-05 20:50:44 -0300

    atualizando ifelse

--- Sesssão 6/controle/operadores_relacionais.php
+++ Sesssão 6/controle/operadores_relacionais.php
@@ -1,30 +1,70 @@
 <div class="titulo"> Operadores Relacionais</div>
 
 <?php
-var_dump(1 == 1) ; //true
+var_dump(1 == 1); //true
 echo '<br>';
-var_dump(1 > 1) ; //false
+var_dump(1 > 1); //false
 echo '<br>';
-var_dump(1 >= 1) ; //true
+var_dump(1 >= 1); //true
 echo '<br>';
-var_dump(4 < 23) ; //true
+var_dump(4 < 23); //true
 echo '<br>';
-var_dump(1 <= 7) ; //true
+var_dump(1 <= 7); //true
 echo '<br>';
-var_dump(1 <> 1) ; //false
+var_dump(1 <> 1); //false
 echo '<br>';
-var_dump(1 != 1) ; //false
+var_dump(1 != 1); //false
 echo '<br>';
 
 
-echo '---------' ;
+echo '---------';
 echo '<br>';
-var_dump(1 == '1').'<br>'; //true
+var_dump(1 == '1') . '<br>'; //true
 echo '<br>';
-var_dump(111 == '111').'<br>'; //true
+var_dump(111 == '111') . '<br>'; //true
 echo '<br>';
-var_dump(111 === '111').'<br>'; //false
+var_dump(111 === '111') . '<br>'; //false
 echo '<br>';
-var_dump(111 != '111').'<br>'; //false
+var_dump(111 != '111') . '<br>'; //false
 echo '<br>';
-var_dump(111 !== '111').'<br>'; //true
+var_dump(111 !== '111') . '<br>'; //true
+
+echo "<p>Relacionais no if/else</p><hr>";
+
+$idade = 25;
+if ($idade < 18) {
+    echo "Menor de idade = $idade anos<br>";
+} else if ($idade <= 65) {
+    echo "Adulto = $idade anos<br>";
+} else {
+    echo "Terceira idade = $idade anos<br>";
+}
+
+echo '<p>Spaceship</p><hr>';
+var_dump(5<=>3);
+echo '<br>';
+var_dump(5<=>5);
+echo '<br>';
+var_dump(1<=>5);
+echo '<br>';
+
+echo '<p>Valores pode ser verdadeiro ou falso</p>';
+var_dump(!!5);
+echo '<br>';
+var_dump(!!0);
+echo '<br>';
+var_dump(!!"");
+echo '<br>';
+var_dump(!!" ");
+echo '<br>';
+
+?>
+<style>
+    p {
+        margin-bottom: 0;
+    }
+
+    hr {
+        margin-top: 0px;
+    }
+</style>
